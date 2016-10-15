8b48e435b952581b541d51ad7476319db67606ab
Saurabh Sood <ssoodindia@gmail.com>
2016-10-15 23:56:58 +0530

    test

--- HackerRank/src/ic/MakingAnagrams.java
+++ HackerRank/src/ic/MakingAnagrams.java
@@ -1,5 +1,27 @@
-package thirtydays;
+package ic;
+
+import java.util.Scanner;
 
 public class MakingAnagrams {
 
+	public static void main(String s[])
+	{
+		 Scanner in = new Scanner(System.in);
+	     StringBuilder a = new StringBuilder(in.next());
+	     StringBuilder b = new StringBuilder(in.next());
+	     
+	     char []c = a.toString().toCharArray();
+	    
+	     for(int i =0; i < c.length ; i++)
+	     {
+	    	 int index = b.indexOf(c[i]+"");
+	    	 if(index != -1)
+	    	 {
+	    		 b.replace(index, index+1, "");
+	    		 index = a.indexOf(c[i]+"");
+	    		 a.replace(index, index+1, "");
+	    	 }	    	
+	     }
+	     System.out.println(b.length()+a.length());	     
+	}		
 }
